6e4ab969a4a2579257a8950ffb661cc72470b0ab
kiel025 <kiel02505@gmail.com>
2022-11-01 11:47:07 -0300

    Entity User and REST working

---
+++ src/main/java/com/kiel/workshopmongo/resources/UserResource.java
@@ -0,0 +1,27 @@
+package com.kiel.workshopmongo.resources;
+
+import com.kiel.workshopmongo.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        User maria = new User("1", "maria brown", "maria@gmail.com");
+        User alex = new User("2", "alex green", "alex@gmail.com");
+
+        List<User> users = new ArrayList<>(Arrays.asList(maria, alex));
+
+        return ResponseEntity.ok().body(users);
+    }
+
+}

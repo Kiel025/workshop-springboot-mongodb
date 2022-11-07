d24eccfe2a82d1c08eab8209fb3553776a35e824
kiel025 <kiel02505@gmail.com>
2022-11-06 23:10:36 -0300

    User insert with POST

--- src/main/java/com/kiel/workshopmongo/services/UserService.java
+++ src/main/java/com/kiel/workshopmongo/services/UserService.java
@@ -1,8 +1,10 @@
 package com.kiel.workshopmongo.services;
 
 import com.kiel.workshopmongo.domain.User;
+import com.kiel.workshopmongo.dto.UserDTO;
 import com.kiel.workshopmongo.repositories.UserRepository;
 import com.kiel.workshopmongo.services.exceptions.ResourceNotFoundException;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,5 +26,13 @@ public class UserService {
         return user.orElseThrow(
                 () -> new ResourceNotFoundException(id)
         );
+    }
+
+    public User insert(User user) {
+        return repository.insert(user);
+    }
+
+    public User fromDTO(@NotNull UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }

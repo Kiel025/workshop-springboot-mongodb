70b6f3877e7814169e45d66b254f3f91ff18c504
kiel025 <kiel02505@gmail.com>
2022-11-08 12:19:44 -0300

    Referencing user posts

--- src/main/java/com/kiel/workshopmongo/config/Instatiation.java
+++ src/main/java/com/kiel/workshopmongo/config/Instatiation.java
@@ -38,5 +38,8 @@ public class Instatiation implements CommandLineRunner {
         Post post2 = new Post(null, LocalDate.parse("23/03/2018", DateTimeFormatter.ofPattern("dd/MM/yyyy")), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }

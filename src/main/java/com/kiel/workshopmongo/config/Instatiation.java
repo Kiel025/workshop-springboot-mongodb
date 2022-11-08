f3ca8e9fb580fba96d94d60748b5cfbceb306d99
kiel025 <kiel02505@gmail.com>
2022-11-08 11:22:50 -0300

    Author data projection with DTO

--- src/main/java/com/kiel/workshopmongo/config/Instatiation.java
+++ src/main/java/com/kiel/workshopmongo/config/Instatiation.java
@@ -2,6 +2,7 @@ package com.kiel.workshopmongo.config;
 
 import com.kiel.workshopmongo.domain.Post;
 import com.kiel.workshopmongo.domain.User;
+import com.kiel.workshopmongo.dto.AuthorDTO;
 import com.kiel.workshopmongo.repositories.PostRepository;
 import com.kiel.workshopmongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,10 +32,11 @@ public class Instatiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, LocalDate.parse("21/03/2018", DateTimeFormatter.ofPattern("dd/MM/yyyy")), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, LocalDate.parse("23/03/2018", DateTimeFormatter.ofPattern("dd/MM/yyyy")), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, LocalDate.parse("21/03/2018", DateTimeFormatter.ofPattern("dd/MM/yyyy")), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, LocalDate.parse("23/03/2018", DateTimeFormatter.ofPattern("dd/MM/yyyy")), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
--- src/main/java/com/kiel/workshopmongo/domain/Post.java
+++ src/main/java/com/kiel/workshopmongo/domain/Post.java
@@ -1,6 +1,7 @@
 package com.kiel.workshopmongo.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.kiel.workshopmongo.dto.AuthorDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -21,11 +22,11 @@ public class Post implements Serializable {
     private LocalDate date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
     public Post(){}
 
-    public Post(String id, LocalDate date, String title, String body, User author) {
+    public Post(String id, LocalDate date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
@@ -65,11 +66,11 @@ public class Post implements Serializable {
         this.body = body;
     }
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 

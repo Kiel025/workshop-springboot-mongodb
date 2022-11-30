fa19dd313f3e1d7286d14b29a198f58ad794a51e
kiel025 <kiel02505@gmail.com>
2022-11-29 22:49:48 -0300

    API documentation with Swagger

--- src/main/java/com/kiel/workshopmongo/resources/UserResource.java
+++ src/main/java/com/kiel/workshopmongo/resources/UserResource.java
@@ -4,6 +4,8 @@ import com.kiel.workshopmongo.domain.Post;
 import com.kiel.workshopmongo.domain.User;
 import com.kiel.workshopmongo.dto.UserDTO;
 import com.kiel.workshopmongo.services.UserService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,20 +21,20 @@ public class UserResource {
     @Autowired
     private UserService service;
 
-    @GetMapping
+    @GetMapping(produces = "application/json")
     public ResponseEntity<List<UserDTO>> findAll() {
         List<User> users = service.findAll();
         List<UserDTO> usersDTO = users.stream().map(UserDTO::new).toList();
         return ResponseEntity.ok().body(usersDTO);
     }
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         UserDTO user = new UserDTO(service.findById(id));
         return ResponseEntity.ok().body(user);
     }
 
-    @PostMapping
+    @PostMapping(consumes = "application/json")
     public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
         User user = service.fromDTO(userDTO);
         user = service.insert(user);
@@ -46,14 +48,14 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("/{id}")
+    @PutMapping(value = "/{id}", consumes = "application/json")
     public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO userDTO) {
         User user = service.fromDTO(userDTO);
         user = service.update(id, user);
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping(value = "/{id}/posts")
+    @GetMapping(value = "/{id}/posts", produces = "application/json")
     public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(user.getPosts());
